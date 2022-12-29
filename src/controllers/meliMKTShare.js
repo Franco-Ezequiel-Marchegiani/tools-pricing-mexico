272b66c254c95ef10046ffbfe1ad6df0764ce6c2
Franco Ezequiel <eze.cavs@hotmail.com>
2022-12-29 12:06:13 -0300

    Removing duplicated data

--- src/controllers/meliMKTShare.js
+++ src/controllers/meliMKTShare.js
@@ -264,7 +264,6 @@ const callMeli = async (urlTodasPublicaciones,headerTodasPublicaciones, paramsTo
 
         //MES ANTERIOR COL
         async function shareMKT_MesAnterior_COL () {
-            celdaForecast.value = date;                                                             //Pisamos el valor y le colocamos el valor que nosotros queremos
             await forecastSheet.saveUpdatedCells();                                                 //Guardamos los cambios y los subimos al Sheet
             
             await consolidadoSheet.loadCells("A2");                                                 //Cargamos la celda a la que modificaremos
