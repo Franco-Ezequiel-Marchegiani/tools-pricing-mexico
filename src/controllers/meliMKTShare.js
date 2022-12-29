6bae992738af290c842b1573ef2ba3b7d44395a0
Franco Ezequiel <eze.cavs@hotmail.com>
2022-12-29 12:04:25 -0300

    Removing duplicated data

--- src/controllers/meliMKTShare.js
+++ src/controllers/meliMKTShare.js
@@ -264,7 +264,6 @@ const callMeli = async (urlTodasPublicaciones,headerTodasPublicaciones, paramsTo
 
         //MES ANTERIOR COL
         async function shareMKT_MesAnterior_COL () {
-        
             const app_VentasSheet =documento.sheetsByTitle['App_Ventas'];
             const r1Sheet =documento.sheetsByTitle['R1'];
             const forecastSheet =documento.sheetsByTitle['Forecast'];
