c17e8c0b0b5706a2118e702abb30d2fcc1def8b4
Franco Ezequiel <eze.cavs@hotmail.com>
2023-01-04 15:24:25 -0300

    Decorating code

--- src/controllers/meliFulfillment.js
+++ src/controllers/meliFulfillment.js
@@ -125,7 +125,7 @@ const consultaAPI  = async (param) => {
                         
                         containerArrayStockProducts.push({                                          //Añadimos al array un objeto con los siguientes valores
                                 MLM:        capturaMLM[indexParams],
-                                id:         element?.id,
+                                id:                             element?.id,
                                 seller_id:                      element?.seller_id,
                                 date_created:                   dateOfProduct,
                                 type:                           element?.type,
